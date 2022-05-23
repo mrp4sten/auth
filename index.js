275d0769c80b6b64234a3ef3e08ef72b0300c2a7
Mauricio Pasten Martinez <mauricio.pasten.martinez@gmail.com>
2022-05-22 22:10:36 -0500

    Middleware validation JWT

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express')
 const mongoose = require('mongoose')
 const bcrypt = require('bcrypt')
 const jwt = require('jsonwebtoken')
-const expressJwt = require('express-jwt')
+const { expressjwt } = require('express-jwt')
 const User = require('./user')
 
 mongoose.connect('mongodb+srv://mrp4sten:mrp4sten@firstcompletenodeapp.ch50k.mongodb.net/?retryWrites=true&w=majority')
@@ -10,12 +10,13 @@ const app = express()
 
 app.use(express.json())
 
+const validateJwt = expressjwt({ secret: 'secret', algorithms: ['HS256'] })
 const signToken = (idUser) => jwt.sign({ _id: idUser }, 'secret')
 
 app.post('/register', async (req, res) => {
     const { body } = req
     try {
-        const isUser = await User.findOne({ email: body.email }) 
+        const isUser = await User.findOne({ email: body.email })
         if (isUser) {
             return res.status(403).send('User is exist')
         }
@@ -23,7 +24,7 @@ app.post('/register', async (req, res) => {
         const salt = await bcrypt.genSalt()
         const hashed = await bcrypt.hash(body.password, salt)
         const user = await User.create({ email: body.email, password: hashed, salt: salt })
-        
+
         const signed = signToken(user._id)
         res.status(201).send(signed)
 
@@ -37,14 +38,14 @@ app.post('/login', async (req, res) => {
     const { body } = req
     try {
         const user = await User.findOne({ email: body.email })
-        if(user) {
+        if (user) {
             const isMatch = await bcrypt.compare(body.password, user.password)
             if (isMatch) {
                 const signed = signToken(user._id)
                 res.status(200).send(signed)
             } else {
                 res.status(403).send('User or password incorrect')
-            }   
+            }
         } else {
             res.status(403).send('User or password incorrect')
         }
@@ -52,6 +53,26 @@ app.post('/login', async (req, res) => {
         console.log(err)
         res.status(500).send(err.message)
     }
+})
+
+const findAndAssingUser = async (req, res, next) => {
+    try {
+        const user = await User.findById(req.auth._id)
+        if (user) {
+            req.auth = user
+            next()
+        } else {
+            return res.status(401).end()
+        }
+    } catch (err) {
+        next(err)
+    }
+}
+
+const isAuthenticated = express.Router().use(validateJwt, findAndAssingUser)
+
+app.get('/JwtValidation', isAuthenticated, (req, res) => {
+    res.send(req.auth)
 })
 
 app.listen(3000, () => {
